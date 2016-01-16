4f7e19506304c9a8da2d99601d8b182f006022c9
ninomi <ten_ten_@hotmail.com>
2016-01-16 14:17:29 +0900

    Add Mongodb Setting

--- docbot.js
+++ docbot.js
@@ -4,9 +4,13 @@ if (!process.env.SLACK_API_TOKEN) {
 }
 
 var Botkit = require('botkit');
+var mongo_storage = require('./node_modules/botkit/lib/storage/mongo_storage.js')({
+  mongo_uri: "mongodb://docbot:docbot@localhost:27017/docbot"
+});
 var os = require('os');
 var controller = Botkit.slackbot({
     debug: true,
+    storage: mongo_storage
 });
 var bot = controller.spawn({
     token: process.env.SLACK_API_TOKEN
